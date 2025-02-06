e0e4d8a39584df5beaba9ea6575c6b71b072950f
RaphaTake <rapha.taketa@gmail.com>
2025-02-06 19:48:25 -0300

    Adicionar arquivos de projeto.

---
+++ ListaBrasil/ListaBrasil/Estados.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ListaBrasil.Modelo;
+
+
+namespace ListaBrasil
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Estados : ContentPage
+    {
+        public Estados()
+        {
+            InitializeComponent();
+            ListaEstados.ItemsSource =
+                Servico.Servico.GetEstados();
+        }
+
+        // private void ListaEstados_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private void SelecaoEStadoAction(object sender, SelectedItemChangedEventArgs args)
+        {
+            Estado estado = (Estado)args.SelectedItem;
+            Navigation.PushAsync(new Municipios(estado));
+        }
+    }
+}
